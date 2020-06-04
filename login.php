1c10a829d86b9f4469118e57d29723b36f10aac9
Thomas60157 <66421835+Thomas60157@users.noreply.github.com>
2020-06-04 15:20:39 +0200

    vue

--- login.php
+++ login.php
@@ -1,81 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title> Formulaire </title>
+  <link rel="stylesheet" type="text/css" href="../lib/styleAll.css">
+
+</head>
+
+<body>
 <?php
-session_start();
-include "../lib/lib.php";
-$i=0;
-
-//variable versification
-$allIsFine = true;
-if(isset($_POST['LOGIN-MAIL'])=== false ){
-    $allIsFine = false;
-}
-if(isset($_POST['LOGIN-MDP'])=== false || $_POST['LOGIN-MDP'] == ""){
-    $allIsFine =false;
-}   
-
-//get info
-if($allIsFine === true){
-    $login = $_POST['LOGIN-MAIL'];
-    $password = $_POST['LOGIN-MDP'];
-    $connection = Connect();
-    $sql = "SELECT * FROM  comptes WHERE email = '".$login."' AND  mdp = '".$password."';";
-     
-    $result = execQuery($connection,$sql);
-    //set variable 
-
-    foreach($result as $row){
-        $mail = $row["email"]; 
-        $Nom = $row["nom"];
-        $Prenom = $row["prenom"];
-        $Actif = $row["actif"];
-        $malvoyant = $row["malvoyant"];
-        $id_langues = $row["id_langues"];
-        $ID_EQUIPES = $row["id_equipes"];
-        $id_statuts = $row["id_statuts"];
-    }
-
-    $sql = "SELECT * FROM  langues WHERE id_langues = '".$id_langues."';";
-     
-    $result = execQuery($connection,$sql);
-
-    foreach($result as $row){
-        $id_langues = $row["nom"];
-    }
-
-    $sql = "SELECT * FROM  statuts WHERE id_statuts = '".$id_statuts."';";
-     
-    $result = execQuery($connection,$sql);
-
-    foreach($result as $row){
-        $id_statuts = $row["nom"];
-    }
-
-
-
-    //versification of account information
-    if(isset($mail)) {
-        if($Actif == 1){
-            echo "connection validée";
-            $_SESSION["mail"]=$mail;
-            $_SESSION["Nom"]=$Nom;
-            $_SESSION["Prenom"]=$Prenom;
-            $_SESSION["statuts"]=$id_statuts;
-            $_SESSION["langues"]=$id_langues;
-            $_SESSION["malvoyant"]=$malvoyant;
-            header('Location: ../vue/compte.php');
-            exit();
-
-        }else{
-            //texte
-            ErreurMSG('ton adm ne t\'aime pas du coup ton compte n\'est pas activé dommage.  on vous aime comme vous etes. <3');
-        }
-    }else{
-        //texte
-        ErreurMSG('ok biche merci <3<3<3 si non c\'est : "le mdp ou le mdp et aussi le MDP ou aussi le FPS ou aussi les FPS ou aussi les deux con qui parle la merde quoi c\'est quoi tout ca la pk je fais de le merde bon aller a plus bon courage a vous. le MDP ou le login est faut ! on vous aime comme vous etes."');
-    }
-
-
-}else{
-    //texte
-    ErreurMSG('apprends a ecrir ton mdp ou ton login fdp !  on vous aime comme vous etes. <3');
-}
+$URL = 'Login';
+include "../lib/NavBar.php";
 ?>
+  <div id="LOGIN">
+    <h1 id="LOGIN-TEXT">Login</h1>
+    <hr id="LOGIN-LINE">
+
+    <form method="POST" action="../Controle/Login.php">
+    //texte
+      <label id="LOGIN-LABEL" name="LOGIN-LABEL-MAIL" >Entez votre adresse Mail</label>
+      <input type="mail" id="LOGIN-MAIL" name="LOGIN-MAIL" placeholder="E-mail" required="required" />
+      
+    //texte
+      <label id="LOGIN-LABEL" name="LOGIN-LABEL-MDP" >Entez votre adresse Mail</label>
+      <input type="password" id="LOGIN-MDP" minlength="8" name="LOGIN-MDP" placeholder="password" required="required" />
+
+      <a href="../vue/SetLogin.php">Forgetten password ?</a>
+      <input type="submit" value="Login">
+    </form>
+  </div>
+</body>
+
+</html>
