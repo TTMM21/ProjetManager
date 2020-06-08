0b6f978822fa96c3ce343fa4f4fc169794c4445b
Marion Porée <m.poree@outlook.fr>
2020-06-08 16:50:46 +0200

    Revert "Ajout de fichiers manquant"
    This reverts commit 7aa58e9486586882f11dfb161e088a35b8236290.

--- lib/lists/getAccountsList.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*Give all the accounts that we need*/
-function getAccountsList() {
-  $req = "SELECT * FROM comptes";
-  $connection = Connect();
-  $result = execQuery($connection, $req);
-  return $result;
-}
-?>
--- lib/lists/getTeamsList.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*Give all the accounts that we need*/
-function getTeamsList($id) {
-  $req = "SELECT * FROM comptes WHERE id_equipes = $id ORDER BY id_equipes";
-  $connection = Connect();
-  $result = execQuery($connection, $req);
-  return $result;
-}
-?>
